9c649cbdc3888227cb79939e74a9e4b4e0d3124c
Ricardo Pereira <rikpereira@gmail.com>
2013-12-30 02:13:45 +0000

    Start

---
+++ mdgp/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mdgp
+//
+//  Created by Ricardo Pereira on 30/12/13.
+//  Copyright (c) 2013 Ricardo Pereira. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
