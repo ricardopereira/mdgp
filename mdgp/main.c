293046c051d5a19767159431ce1f7bbc1278a0cd
Ricardo Pereira <rikpereira@gmail.com>
2014-01-01 20:03:28 +0000

    Suporte para vários algoritmos (enumerado TipoAlgoritmo)

--- mdgp/main.c
+++ mdgp/main.c
@@ -5,11 +5,21 @@
 #include "algoritmo.h"
 #include "utils.h"
 
+enum TipoAlgoritmo
+{
+    algTrepaColinas,
+    algTrepaColinasProb,
+    algRecristalizacaoSimulada,
+    algTabu,
+    algGeneticoPorTorneio
+};
+
 #define DEFAULT_RUNS 10
-#define DEFAULT_FILE "RanInt_n010_ss_01.txt"
+#define DEFAULT_FILE "RanInt_n012_ss_01.txt"
 
 int main(int argc, char *argv[])
 {
+    enum TipoAlgoritmo algoritmo;
     char nome_fich[100];
     int *sol, *best;
     int **dist;
@@ -31,77 +41,103 @@ int main(int argc, char *argv[])
 		runs = DEFAULT_RUNS;
         printf("Nome do Ficheiro: ");
         
-        //Teste
-        /*gets(nome_fich);*/
+        // Teste
+        //gets(nome_fich);
         strcpy(nome_fich,DEFAULT_FILE);
-        num_iter = 10000;
     }
-  
+    
 	if(runs <= 0)
 		return 0;
 	
 	init_rand();
     
+    // Configuracao
+    algoritmo = algTrepaColinas;
+    num_iter = 10000;
     
-    /* Preenche matriz de adjacencias */
+    // Preenche matriz de distancias
     dist = init_dados(nome_fich, &m, &g);
     
     printf("Elementos: %d\n",m);
     printf("Sub-conjuntos: %d\n",g);
     
+    // Confirmar valores da matriz
     mostra_matriz(dist,m);
     printf("\n");
     
-  	
-	sol = malloc(sizeof(int)*m);
-	best = malloc(sizeof(int)*m);
-	
-	if(sol == NULL || best == NULL)
-	{
-		printf("Erro na alocacao de memoria");
-		exit(1);
-	}
-	
-	for(k=0; k<runs; k++)
-	{
-		// Gerar solucao inicial
-		gera_sol_inicial(sol, m, g);
-        
-        
-		// Trepa colinas simples
-		custo = trepa_colinas(sol, dist, m, g, num_iter);
-    
-		// Trepa colinas probabilístico
-		//custo = tc_prob(sol, grafo, vert, num_iter);
-
-		// Trepa colinas Recristalização Simulada
-		//custo = tc_simulated_annealing(sol, grafo, vert, num_iter);
-        
-        
-		// Escreve resultados da repeticao k
-		printf("\nRepeticao %d:", k);
-		escreve_sol(sol, m, g);
-		printf("Custo final: %2d\n", custo);
-		
-		mbf += custo;
-		if(k==0 || best_custo < custo)
-		{
-			best_custo = custo;
-			copia(best, sol, m);
-		}
-    } 
-	
-	// Escreve eresultados globais
-	printf("\n\nMBF: %f\n", mbf/k);
-	printf("\nMelhor solucao encontrada");
-	escreve_sol(best, m, g);
-	printf("Custo final: %2d\n", best_custo);
-	
+    // Maximizacao
+    switch (algoritmo)
+    {
+        case algTrepaColinas:
+        case algTrepaColinasProb:
+        case algRecristalizacaoSimulada:
+        case algTabu:
+            sol = malloc(sizeof(int)*m);
+            best = malloc(sizeof(int)*m);
+            
+            if(sol == NULL || best == NULL)
+            {
+                printf("Erro na alocacao de memoria");
+                exit(1);
+            }
+            
+            for(k=0; k<runs; k++)
+            {
+                // Gerar solucao inicial
+                gera_sol_inicial(sol, m, g);
+                
+                switch (algoritmo)
+                {
+                    case algTrepaColinas:
+                        // Trepa colinas simples
+                        custo = trepa_colinas(sol, dist, m, g, num_iter);
+                        break;
+                    case algTrepaColinasProb:
+                        // Trepa colinas probabilístico
+                        //custo = tc_prob(sol, grafo, vert, num_iter);
+                        break;
+                    case algRecristalizacaoSimulada:
+                        // Trepa colinas Recristalização Simulada
+                        //custo = tc_simulated_annealing(sol, grafo, vert, num_iter);
+                        break;
+                    case algTabu:
+                        
+                        break;
+                    default:
+                        exit(0);
+                }
+                
+                // Escreve resultados da repeticao k
+                printf("\nRepeticao %d:", k);
+                escreve_sol(sol, m, g);
+                printf("Custo final: %2d\n", custo);
+                
+                mbf += custo;
+                if(k==0 || best_custo < custo)
+                {
+                    best_custo = custo;
+                    copia(best, sol, m);
+                }
+            } 
+            
+            // Escreve eresultados globais
+            printf("\n\nMBF: %f\n", mbf/k);
+            printf("\nMelhor solucao encontrada");
+            escreve_sol(best, m, g);
+            printf("Custo final: %2d\n", best_custo);
+            
+            free(sol);
+            free(best);
+            break;
+            
+        case algGeneticoPorTorneio:
+            
+            break;
+    }
+  		
     // Libertar memoria
     for (i=0; i<(m-1); i++)
         free(dist[i]);
     free(dist);
-    free(sol);    
-	free(best);
-    return 0;    
+    return 0;
 }
